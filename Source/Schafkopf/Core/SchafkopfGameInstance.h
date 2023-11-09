671b357952c06ad4c662a991bddc96d99c82f03a
Valentin Schmalz <vschmalz@hm.edu>
2023-11-09 22:08:40 +0100

    Created SchafkopfGameInstance class

---
+++ Source/Schafkopf/Core/SchafkopfGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SchafkopfGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCHAFKOPF_API USchafkopfGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
