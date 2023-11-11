1d1a48fe30ee91bb2863acad14993ba8731ead9e
Valentin Schmalz <vschmalz@hm.edu>
2023-11-11 17:02:32 +0100

    Created SchafkopfPlayer class

---
+++ Source/Schafkopf/Core/SchafkopfPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SchafkopfPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCHAFKOPF_API USchafkopfPlayer : public UObject
+{
+	GENERATED_BODY()
+	
+};
