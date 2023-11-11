1125ffb7a4bc0c496d17fb1f53d4533821560b61
Valentin Schmalz <vschmalz@hm.edu>
2023-11-11 14:19:59 +0100

    Created Lobby class

---
+++ Source/Schafkopf/Core/Lobby.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Lobby.generated.h"
+
+/**
+ * The Lobby class.
+ */
+UCLASS()
+class SCHAFKOPF_API ULobby : public UObject
+{
+	GENERATED_BODY()
+	
+};
