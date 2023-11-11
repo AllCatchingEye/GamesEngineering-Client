58d9c2a3de80b46d49f1ffcb9ec4a563e15e8e28
Valentin Schmalz <vschmalz@hm.edu>
2023-11-11 17:06:50 +0100

    Added an ID property and a factory method

--- Source/Schafkopf/Core/SchafkopfPlayer.h
+++ Source/Schafkopf/Core/SchafkopfPlayer.h
@@ -13,5 +13,28 @@ UCLASS()
 class SCHAFKOPF_API USchafkopfPlayer : public UObject
 {
 	GENERATED_BODY()
-	
+
+private:
+	/** The ID of the Player. The ID is immutable. */
+	FName Id;
+
+public:
+	/**
+	 * Returns the ID of the Player.
+	 *
+	 * The ID of the Player is immutable.
+	 *
+	 * @return The ID of the Player.
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FName GetId();
+
+	/**
+	 * Creates a new Player.
+	 *
+	 * @param Id - The ID of the Player.
+	 * @return A new Player object.
+	 */
+	UFUNCTION(BlueprintCallable)
+	static USchafkopfPlayer* Make(const FName Id);
 };
