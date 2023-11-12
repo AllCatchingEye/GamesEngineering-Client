e1370ebe2ee9b7c7d9acc3fc2866f28a5dce7d1b
Valentin Schmalz <vschmalz@hm.edu>
2023-11-12 22:56:29 +0100

    Fixed project structure
    The project now adheres to the Unreal Engine's structure of organizing headers and source files into public and private folders.
    
    Only things that should be visible outside the Schafkopf module belong in the public folder. All other things, generally mostly source (.cpp) files belong in the private folder.

--- Source/Schafkopf/Private/Core/Cards/Card.cpp
+++ Source/Schafkopf/Private/Core/Cards/Card.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Card.h"
+#include "Core/Cards/Card.h"
 
 ECardSuit UCard::getSuit() const {
 	return this->Suit;
