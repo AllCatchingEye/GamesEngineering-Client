a247c62f8c216da597267720e094948c8aff834c
Valentin Schmalz <vschmalz@hm.edu>
2023-11-16 18:55:20 +0100

    Added static texture array

--- Source/Schafkopf/Private/Cards/Card.cpp
+++ Source/Schafkopf/Private/Cards/Card.cpp
@@ -48,3 +48,63 @@ void ACard::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+const TStaticArray<UTexture2D*, ACard::CARD_TEXTURES_AMOUNT> ACard::CARD_TEXTURES = []() -> TStaticArray<UTexture2D*, ACard::CARD_TEXTURES_AMOUNT>
+{
+	TStaticArray<UTexture2D*, ACard::CARD_TEXTURES_AMOUNT> Textures;
+
+	// Fetch the base card texture.
+	// This texture is used for unknown cards.
+	ConstructorHelpers::FObjectFinder<UTexture2D> TextureUnknown(
+		TEXT("Texture2D'/Game/Cards/T_CardBack.T_CardBack'")
+	);
+
+	if (TextureUnknown.Succeeded())
+	{
+		Textures[0] = TextureUnknown.Object;
+	}
+
+	// Fetch all actual card textures.
+	// The texture location string has the following form:
+	// "Texture2D'/Game/FOLDER/SUB_FOLDER/.../TEXTURE_NAME.TEXTURE_NAME'"
+	const FString CardTextureLocationStart = TEXT("Texture2D'/Game/Cards/");
+	const FString CardTextureLocationConcatenator = TEXT(".");
+	const FString CardTextureLocationEnd = TEXT("'");
+	const FString CardTextureNameBase = TEXT("T_CardFront_");
+	const FString CardTextureNameSeparator = TEXT("_");
+	FString CardTextureNameFull;
+	FString CardTextureLocationFull;
+	uint8 EnumValueSuit;
+	uint8 EnumValueRank;
+	for (ECardSuit CardSuit : TEnumRange<ECardSuit>())
+	{
+		EnumValueSuit = (uint8)CardSuit;
+
+		for (ECardRank CardRank : TEnumRange<ECardRank>())
+		{
+			// Construct the card texture name.
+			EnumValueRank = (uint8)CardRank;
+			CardTextureNameFull = CardTextureNameBase;
+			CardTextureNameFull += FString::FromInt(EnumValueSuit);
+			CardTextureNameFull += CardTextureNameSeparator;
+			CardTextureNameFull += FString::FromInt(EnumValueRank);
+
+			// Construct the card texture location.
+			CardTextureLocationFull = CardTextureLocationStart;
+			CardTextureLocationFull += CardTextureNameFull;
+			CardTextureLocationFull += CardTextureLocationConcatenator;
+			CardTextureLocationFull += CardTextureNameFull;
+			CardTextureLocationFull += CardTextureLocationEnd;
+
+			// Fetch the card texture.
+			ConstructorHelpers::FObjectFinder<UTexture2D> Texture(*CardTextureLocationFull);
+
+			if (Texture.Succeeded())
+			{
+				Textures[EnumValueSuit * EnumValueRank] = Texture.Object;
+			}
+		}
+	}
+
+	return Textures;
+}();
