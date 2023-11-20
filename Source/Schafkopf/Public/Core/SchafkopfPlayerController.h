7db7204c45815f348289dbffd267bd6220df2af6
Valentin Schmalz <vschmalz@hm.edu>
2023-11-20 13:05:15 +0100

    Created SchafkopfPlayerController class

---
+++ Source/Schafkopf/Public/Core/SchafkopfPlayerController.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+
+#include "SchafkopfPlayerController.generated.h"
+
+/**
+ * The SchafkopfPlayerController class.
+ */
+UCLASS()
+class SCHAFKOPF_API ASchafkopfPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
