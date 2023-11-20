db730f572ae443e245e5d22bf5f125f17f9bdb6e
Valentin Schmalz <vschmalz@hm.edu>
2023-11-20 18:07:50 +0100

    Created SchafkopfGameMode class

---
+++ Source/Schafkopf/Public/Core/SchafkopfGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SchafkopfGameMode.generated.h"
+
+/**
+ * The SchafkopfGameMode class.
+ */
+UCLASS()
+class SCHAFKOPF_API ASchafkopfGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	ASchafkopfGameMode();
+};
