751689129a201301d005cfbc49208639c85b3f3b
Valentin Schmalz <vschmalz@hm.edu>
2023-11-20 21:06:34 +0100

    Added methods to show UI widgets

--- Source/Schafkopf/Public/Core/SchafkopfPlayerController.h
+++ Source/Schafkopf/Public/Core/SchafkopfPlayerController.h
@@ -24,8 +24,43 @@ public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="UI")
 	TSubclassOf<UUserWidget> WidgetClass;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI")
+	TSubclassOf<UUserWidget> WidgetClassWantsToPlay;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI")
+	TSubclassOf<UUserWidget> WidgetClassGameGroupSelect;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI")
+	TSubclassOf<UUserWidget> WidgetClassGameTypeSelect;
+
 	UPROPERTY()
 	UUserWidget* WidgetInstance;
+
+	UPROPERTY()
+	UUserWidget* WidgetInstanceWantsToPlay;
+
+	UPROPERTY()
+	UUserWidget* WidgetInstanceGameGroupSelect;
+
+	UPROPERTY()
+	UUserWidget* WidgetInstanceGameTypeSelect;
+
+	/** Shows the WantsToPlay widget. */
+	void ShowWidgetWantsToPlay();
+
+	/**
+	 * Shows the GameGroupSelect widget.
+	 * 
+	 * @param Groups - The available groups.
+	 */
+	void ShowWidgetGameGroupSelect(const TArray<FString> Groups);
+
+	/**
+	 * Shows the GameTypeSelect widget.
+	 *
+	 * @param Types - The available types.
+	 */
+	void ShowWidgetGameTypeSelect(const TArray<FString> Types);
 
 private:
 	ASchafkopfCharacter* PosessedPawn;
