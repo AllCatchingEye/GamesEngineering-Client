65cbe852aca4aa11f672aa41c6c7671441aabf82
Valentin Schmalz <vschmalz@hm.edu>
2023-11-21 01:25:16 +0100

    Created base widgets

---
+++ Source/Schafkopf/Public/UI/GameGroupSelectWidget.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Blueprint/WidgetTree.h"
+#include "Components/VerticalBox.h"
+
+#include "WsMessage.h"
+#include "GameGroupSelectWidget.generated.h"
+
+/**
+ * The GameGroupSelectWidget class.
+ */
+UCLASS()
+class SCHAFKOPF_API UGameGroupSelectWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void UpdateFields(const TArray<FWSGameTypeWithSuit>& Groups);
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	UVerticalBox* VerticalBox;
+};
